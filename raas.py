abf81a83bb4398f682c9a4cb14395cb65a0cd5fa
pouyarb <48477477+pouyarb@users.noreply.github.com>
2020-01-19 16:47:32 +0330

    first commit by pouya

---
+++ raas.py
@@ -0,0 +1,105 @@
+import pandas as pd
+
+class raas:
+    def __init__(self,key = 0):
+        self.key=key
+
+
+class hesabbanki(raas):
+
+    dic={}
+
+    def __init__(self,codemeli=0,namebank=0,shshaba=0,shhesab=0):
+        super(hesabbanki,self).__init__(shshaba)
+        self.codemeli=codemeli
+        self.namebank=namebank
+        self.shhesab=shhesab
+        self.in_tarakonesh=[]
+        self.out_tarakonesh=[]
+
+
+class khane(raas):
+
+    dic={}
+
+    def __init__(self,codemeli=0,gheymat=0,codeposti=0,metraj=0,addres=0):
+        super(khane,self).__init__(codeposti)
+        self.codemeli=codemeli
+        self.gheymat=gheymat
+        self.metraj=metraj
+        self.addres=addres
+
+
+class mashin(raas):
+
+    dic={}
+
+    def __init__(self,pelak=0,codemeli=0,model=0,rang=0):
+        super(mashin,self).__init__(pelak)
+        self.codemeli=codemeli
+        self.model=model
+        self.rang=rang
+
+
+class hamrah(raas):
+
+    dic={}
+
+    def __init__(self,codemeli=0,sim=0,operator=0):
+        super(hamrah,self).__init__(sim)
+        self.codemeli=codemeli
+        self.operator=operator
+        self.in_tamas=[]
+        self.out_tamas=[]
+
+class shakhs(raas):
+
+    dic={}
+
+    def __init__(self,name=0,familyname=0,codemeli=0,tarikhtavalod=0,mahaletavalod=0,mahalekar=0):
+        super(shakhs,self).__init__(codemeli)
+        self.name=name
+        self.familyname=familyname
+        self.tarikhtavalod=tarikhtavalod
+        self.mahaletavalod=mahaletavalod
+        self.mahalekar=mahalekar
+        self.mashinha=[]
+        self.khaneha=[]
+        self.family=[]
+
+ 
+hesabbanki_csv = pd.read_csv('accounts.csv')
+hesabbanki_csv.columns = ['codemeli' , 'namebank', 'shshaba' , 'shhesab']
+
+mashin_csv = pd.read_csv('cars.csv')
+mashin_csv.columns= ['model' ,'codemeli','pelak','rang']
+
+khane_csv = pd.read_csv('homes.csv')
+khane_csv.columns = ['codemeli', 'gheymat','codeposti','metraj','addres']
+
+hamrah_csv = pd.read_csv('phones.csv')
+hamrah_csv.columns = ['codemeli','sim','operator']
+
+shakhs_csv = pd.read_csv('people.csv')
+shakhs_csv.drop(columns= 'address',axis =1,inplace = True)
+shakhs_csv.columns = ['name','familyname','codemeli','tarikhtavalod','mahaletavalod','mahalekar']
+
+l=len(shakhs_csv)
+for i in range(l):
+    shakhs.dic[str(shakhs_csv[i:i + 1]['codemeli'].iloc[0])] =shakhs(shakhs_csv[i:i + 1]['name'].iloc[0], shakhs_csv[i:i + 1]['familyname'].iloc[0], shakhs_csv[i:i + 1]['tarikhtavalod'].iloc[0], shakhs_csv[i:i + 1]['mahaletavalod'].iloc[0], shakhs_csv[i:i + 1]['mahalekar'].iloc[0])
+
+l=len(khane_csv)
+for i in range(l):
+    khane.dic[khane_csv[i:i + 1]['codeposti'].iloc[0]] =khane(khane_csv[i:i + 1]['codemeli'].iloc[0], khane_csv[i:i + 1]['gheymat'].iloc[0], khane_csv[i:i + 1]['codeposti'].iloc[0], khane_csv[i:i + 1]['metraj'].iloc[0], khane_csv[i:i + 1]['addres'].iloc[0])
+
+l=len(mashin_csv)
+for i in range(l):
+    mashin.dic[mashin_csv[i:i + 1]['pelak'].iloc[0]] =mashin(mashin_csv[i:i + 1]['pelak'].iloc[0], mashin_csv[i:i + 1]['codemeli'].iloc[0], mashin_csv[i:i + 1]['model'].iloc[0], mashin_csv[i:i + 1]['rang'].iloc[0])
+
+l =len(hamrah_csv)
+for i in range(l):
+    hamrah.dic[hamrah_csv[i:i + 1]['sim'].iloc[0]] =hamrah(hamrah_csv[i:i + 1]['codemeli'].iloc[0], hamrah_csv[i:i + 1]['sim'].iloc[0], hamrah_csv[i:i + 1]['operator'].iloc[0])
+
+l=len(hesabbanki_csv)
+for i in range(l):
+    hesabbanki.dic[str(hesabbanki_csv[i:i + 1]['shhesab'].iloc[0])] =hesabbanki(str(hesabbanki_csv[i:i + 1]['codemeli'].iloc[0]), str(hesabbanki_csv[i:i + 1]['namebank'].iloc[0]), hesabbanki_csv[i:i + 1]['shshaba'].iloc[0], str(hesabbanki_csv[i:i + 1]['shhesab'].iloc[0]))
---
+++ yal.py
@@ -0,0 +1,89 @@
+from raas import *
+import pandas as pd
+
+class yal:
+    def __init__(self,key = 0,next = None, prev = None):
+        self.key=key
+        self.next= next
+        self.prev=prev
+
+class malekiat(yal):
+
+    dic={}
+
+    def __init__(self,sabteahval=0, tamalok=0, pardakhti=0, next=None, prev=None):
+        super(malekiat,self).__init__(sabteahval,next,prev)
+        self.tamalok=tamalok
+        self.pardakhti=pardakhti
+          
+class tarakonesh(yal):
+
+    dic={}
+
+    def __init__(self,shomare=0, zaman=0, mablagh=0, next=None, prev=None):
+        super(tarakonesh,self).__init__(shomare,next,prev)
+        self.zaman=zaman
+        self.mablagh=mablagh
+
+class tamas(yal):
+
+    dic={}
+
+    def __init__(self,shomare=0,zaman=0,modat=0, next=None, prev=None):
+        super(tamas,self).__init__(shomare,next,prev)
+        self.zaman=zaman
+        self.modat=modat
+
+class family(yal):
+
+    dic={}
+    
+    def __init__(self,tarkibi=0, nesbat=0, shoruenesbat=0, next=None, prev=None):
+        super(family,self).__init__(tarkibi,next,prev)
+        self.nesbat=nesbat
+        self.shoruenesbat=shoruenesbat
+
+
+tamsa_csv=pd.read_csv('calls.csv')
+tamsa_csv.columns=['prev','next','shomare','zaman','modat']
+
+malekiat_csv = pd.read_csv('ownerships.csv')
+malekiat_csv.columns = ['prev','next','sabteahval','tamalok', 'pardakhti']
+
+family_csv = pd.read_csv('relationships.csv')
+family_csv.columns = ['prev','next','nesbat','shoruenesbat']
+
+tarakonesh_csv = pd.read_csv('transactions.csv')
+tarakonesh_csv.columns = ['prev','next','shomare','zaman','mablagh']
+
+l=len(tamsa_csv)
+for i in range(l):
+    a=tamsa_csv[i:i+1]['shomare'].iloc[0]
+    tamas.dic[a] =tamas(a, tamsa_csv[i:i + 1]['zaman'].iloc[0], tamsa_csv[i:i + 1]['modat'].iloc[0], tamsa_csv[i:i + 1]['next'].iloc[0], tamsa_csv[i:i + 1]['prev'].iloc[0])
+    hamrah.dic[tamsa_csv[i:i + 1]['next'].iloc[0]].in_tamas.append(a)
+    hamrah.dic[tamsa_csv[i:i + 1]['prev'].iloc[0]].out_tamas.append(a)
+
+
+
+l=len(tarakonesh_csv)
+for i in range(1,l):
+    a=str(tamsa_csv[i:i+1]['shomare'].iloc[0])
+    tarakonesh.dic[a] =tarakonesh(a, tarakonesh_csv[i:i + 1]['zaman'].iloc[0], str(tarakonesh_csv[i:i + 1]['mablagh'].iloc[0]), str(tarakonesh_csv[i:i + 1]['next'].iloc[0]), str(tarakonesh_csv[i:i + 1]['prev'].iloc[0]))
+    hesabbanki.dic[str(tarakonesh_csv[i:i + 1]['next'].iloc[0])].in_tarakonesh.append(a)
+    hesabbanki.dic[str(tarakonesh_csv[i:i + 1]['prev'].iloc[0])].out_tarakonesh.append(a)
+
+l=len(malekiat_csv)
+for i in range(l):
+    a=malekiat_csv[i:i+1]['sabteahval'].iloc[0]
+    malekiat.dic[a] =malekiat(a, malekiat_csv[i:i + 1]['tamalok'].iloc[0], malekiat_csv[i:i + 1]['pardakhti'].iloc[0], malekiat_csv[i:i + 1]['next'].iloc[0], malekiat_csv[i:i + 1]['prev'].iloc[0])
+    if malekiat_csv[i:i+1]['next'].iloc[0] in khane.dic:
+        shakhs.dic[str(malekiat_csv[i:i + 1]['prev'].iloc[0])].khaneha.append(a)
+    else:
+        shakhs.dic[str(malekiat_csv[i:i + 1]['prev'].iloc[0])].mashinha.append(a)
+
+l=len(family_csv)
+for i in range(l):
+    a=str(family_csv[i:i+1]['prev'].iloc[0])+ "+" +str(family_csv[i:i+1]['next'].iloc[0])
+    family.dic[a] =family(a, family_csv[i:i + 1]['nesbat'].iloc[0], family_csv[i:i + 1]['shoruenesbat'].iloc[0], family_csv[i:i + 1]['next'].iloc[0], family_csv[i:i + 1]['prev'].iloc[0])
+    shakhs.dic[str(family_csv[i:i + 1]['prev'].iloc[0])].family.append(family_csv[i:i + 1]['next'].iloc[0])
+
